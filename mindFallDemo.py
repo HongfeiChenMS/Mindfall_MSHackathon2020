33cc32673585214d69eda3dc9f792e0299319711
Hongfei Chen <t-hoc@microsoft.com>
2020-07-29 16:16:39 -0700

    button issue resolved

--- mindFallDemo.py
+++ mindFallDemo.py
@@ -8,6 +8,8 @@ import time
 import imageio # pip install imageio; pip install imageio-ffmpeg
 from functools import partial
 
+STARTBREAK = False
+
 # Get absolute path to resource, works for dev and for PyInstaller
 def resource_path(relative_path):
     try:
@@ -18,63 +20,93 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 def updateClock(frameNum):
-    # remove notification
-    cv.itemconfig(notificationIt, text = '')
-    if (frameNum < len(starSky_videoStream)):
-        t = time.time()
-        current = time.strftime('%H:%M', time.localtime(t))
-        # show notification at certain time
-        # TODO: change the time (using seconds to demo for mow)
-        if time.strftime('%S', time.localtime(t)) == '00':
-            window.after(1, showNotification, 'Good \nMorning!', frameNum, 0)
-            return
-        elif time.strftime('%S', time.localtime(t)) == '20':
-            window.after(1, showNotification, 'Time for \nLunch!', frameNum, 0)
-            return
-        elif time.strftime('%S', time.localtime(t)) == '40':
-            window.after(1, showNotification, 'All set for \nthe day?', frameNum, 0)
-            return
-        # update clock
-        cv.itemconfig(clockIt, text=current, font=("Arial", 40, "bold"))
-        # reset clock position
-        cv.coords(clockIt, 250, 350)
-        cv.itemconfig(imgIt, image = starSky_videoStream[frameNum])
+    if (not STARTBREAK):
+        cv.itemconfig(end_break, width=0, height=0)
+        # remove notification
+        cv.itemconfig(notificationIt, text = '')
+        cv.coords(imgIt, -300, -150)
+        if (frameNum < len(starSky_videoStream)):
+            t = time.time()
+            current = time.strftime('%H:%M', time.localtime(t))
+            # show notification at certain time
+            # TODO: change the time (using seconds to demo for mow)
+            if time.strftime('%S', time.localtime(t)) == '00':
+                window.after(1, showNotification, 'Good \nMorning!', frameNum, 0)
+                return
+            elif time.strftime('%S', time.localtime(t)) == '20':
+                window.after(1, showNotification, 'Time for \nLunch!', frameNum, 0)
+                return
+            elif time.strftime('%S', time.localtime(t)) == '40':
+                window.after(1, showNotification, 'All set for \nthe day?', frameNum, 0)
+                return
+            # update clock
+            cv.itemconfig(clockIt, text=current, font=("Arial", 40, "bold"))
+            # reset clock position
+            cv.coords(clockIt, 250, 350)
+            cv.itemconfig(imgIt, image = starSky_videoStream[frameNum])
 
-        window.after(100, updateClock, frameNum+1)
-    else:
-        # restart the video
-        # TODO: add new video
-        window.after(1, updateClock, 0)
+            window.after(100, updateClock, frameNum+1)
+        else:
+            # restart the video
+            # TODO: add new video
+            window.after(1, updateClock, 0)
 
 def showNotification(message, frameNum, timeCnt):
-    if timeCnt > 30:
-        # updateClock(frameNum)
-        window.after(1, updateClock, frameNum)
-        return
-    if (frameNum < len(starSky_videoStream)):
-        cv.itemconfig(imgIt, image = starSky_videoStream[frameNum])
-        # show notification message
-        cv.itemconfig(notificationIt, text = message)
-        # update clock and change font and position
-        t = time.time()
-        current = time.strftime('%H:%M', time.localtime(t))
-        cv.itemconfig(clockIt, text=current, font=("Arial", 15))
-        cv.coords(clockIt, 100, 50)
-        window.after(100, showNotification, message, frameNum+1, timeCnt+1)
-    else:
-        window.after(1, showNotification, message, 0, timeCnt)
+    if (not STARTBREAK):
+        if timeCnt > 30:
+            # updateClock(frameNum)
+            window.after(1, updateClock, frameNum)
+            return
+        if (frameNum < len(starSky_videoStream)):
+            cv.itemconfig(imgIt, image = starSky_videoStream[frameNum])
+            # show notification message
+            cv.itemconfig(notificationIt, text = message)
+            # update clock and change font and position
+            t = time.time()
+            current = time.strftime('%H:%M', time.localtime(t))
+            cv.itemconfig(clockIt, text=current, font=("Arial", 15))
+            cv.coords(clockIt, 100, 50)
+            # end break btn
+            cv.itemconfig(end_break, width=0, height=0)
+            window.after(100, showNotification, message, frameNum+1, timeCnt+1)
+        else:
+            window.after(1, showNotification, message, 0, timeCnt)
+
+# def startBreak(frameNum):
+#     # remove notification
+#     cv.itemconfig(notificationIt, text = '')
+    
+#     if (frameNum < len(break_Stream)):
+#         cv.itemconfig(imgIt, image = break_Stream[frameNum])
+#         # cv.move(imgIt, 400, 200)
+#         # cv.create_window(10, 10, anchor ='nw', window = break_Stream[frameNum])
+#         window.after(100, startBreak, frameNum+1)
+#     else:
+#         window.after(1, updateClock, 0)
 
 def startBreak(frameNum):
+    global STARTBREAK
+    STARTBREAK = True
     # remove notification
     cv.itemconfig(notificationIt, text = '')
     
     if (frameNum < len(break_Stream)):
+        # hide start break button
+        cv.itemconfig(break_button, width=0, height=0)
         cv.itemconfig(imgIt, image = break_Stream[frameNum])
+        cv.coords(imgIt, 0, 75)
         # cv.move(imgIt, 400, 200)
         # cv.create_window(10, 10, anchor ='nw', window = break_Stream[frameNum])
+        # update clock and change font and position
+        t = time.time()
+        current = time.strftime('%H:%M', time.localtime(t))
+        cv.itemconfig(clockIt, text=current)
         window.after(100, startBreak, frameNum+1)
     else:
-        window.after(1, updateClock, 0)
+        STARTBREAK = False
+        cv.itemconfig(notificationIt, text = 'Break Over')
+        
+        
 
 # create a tkinter window which we are going to place our pet
 window = tk.Tk()
@@ -119,12 +151,14 @@ imgIt = cv.create_image(-300, -150, image=bg_image, anchor='nw')
 # add clock text item to canvas
 clockIt = cv.create_text(150, 20, text="", fill="#FBAC95", anchor='n')
 # add notification item to canvas
-notificationIt = cv.create_text(250, 250, text="", font=("Arial", 40, "bold"), fill="#FBAC95", anchor='n')
+notificationIt = cv.create_text(250, 250, text="", font=("Arial", 30, "bold"), fill="#FBAC95", anchor='n')
 
 # create break button
 break_button = tk.Button(window, text = 'Start Break!', bg = "#FBAC95", command = partial(startBreak,10), anchor = 'n')
 break_window = cv.create_window(350, 10, anchor ='nw', window = break_button)
-
+end_break = tk.Button(window, text = 'Return to Main', bg = "#FBAC95", command = partial(updateClock, 0), anchor = 'n')
+endbreak_window = cv.create_window(350, 10, anchor ='nw', window = end_break)
 # start the clock
-window.after(1, updateClock, 0)
+# window.after(1, updateClock, 0)
+updateClock(0)
 window.mainloop()
